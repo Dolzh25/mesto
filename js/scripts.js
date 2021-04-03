380dabebeabef5965005891839378113bc155ed8
Dolzh25 <gidravlika87@yandex.ru>
2021-04-03 14:52:32 +0300

    Оживляет попап

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+(() => {
+  const editProfile = document.querySelector('.user__button-edit');
+  const popup = document.querySelector('.popup');
+  const name = document.querySelector('.user__name');
+  const about = document.querySelector('.user__about');
+  const formName = popup.querySelector('#name');
+  const formAbout = popup.querySelector('#about');
+  const form = popup.querySelector('.form');
+  const popupClose = popup.querySelector('.popup__close');
+
+  const openPopup = () => {
+    popup.classList.add('popup_opened');
+
+    formName.value = getPopupInputValue(name);
+    formAbout.value = getPopupInputValue(about);
+
+    form.addEventListener('submit', setNewValues);
+
+    popupClose.addEventListener('click', closePopup);
+  };
+
+  const setNewValues = (evt) => {
+    evt.preventDefault();
+    name.textContent = formName.value;
+    about.textContent = formAbout.value;
+    closePopup();
+  };
+
+  const closePopup = () => {
+    popup.classList.remove('popup_opened');
+    popupClose.removeEventListener('click', closePopup);
+    form.removeEventListener('submit', setNewValues);
+  };
+
+  const getPopupInputValue = input => input.textContent;
+
+  editProfile.addEventListener('click', openPopup);
+})();
