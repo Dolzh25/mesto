afa81517a769df8f841d74cf457afb9eb6d6fd70
Dolzh25 <gidravlika87@yandex.ru>
2021-04-05 17:38:36 +0300

    Добавляет карточки на страницу с помощью скрипта

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,56 @@
   const formAbout = popup.querySelector('#about');
   const form = popup.querySelector('.form');
   const popupClose = popup.querySelector('.popup__close');
+  const galleryList = document.querySelector('.gallery__list');
+  const cardTemplate = document.querySelector('#post').content;
+
+  const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+  const createCard = (name, link) => {
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardImg = cardElement.querySelector('.post__image');
+    const cardTitle = cardElement.querySelector('.post__title');
+
+    cardImg.src = link;
+    cardImg.alt = name;
+    cardTitle.textContent = name;
+    return cardElement;
+  };
+
+  const renderCards = () => {
+    initialCards.forEach((card) => {
+      const newCard = createCard(card.name, card.link);
+      galleryList.append(newCard);
+    });
+  };
+
+  renderCards();
+
 
   const openPopup = () => {
     popup.classList.add('popup_opened');
