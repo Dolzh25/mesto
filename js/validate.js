1ee86a77b06f586367fee1090e56dda6ab0d39fb
Dolzh25 <gidravlika87@yandex.ru>
2021-04-18 12:37:35 +0300

    Добавляет валидацию форм

---
+++ js/validate.js
@@ -0,0 +1,70 @@
+(() => {
+  const formSelectors = {
+    formSelector: '.form',
+    inputSelector: '.form__input',
+    submitButtonSelector: '.form__button-submit',
+    inactiveButtonClass: 'form__button-submit_disabled',
+    inputErrorClass: 'form__input_error',
+    errorClass: 'form__input-error_active'
+  }
+
+  const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(formSelectors.inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(formSelectors.errorClass);
+  };
+
+  const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(formSelectors.inputErrorClass);
+    errorElement.classList.remove(formSelectors.errorClass);
+    errorElement.textContent = '';
+  };
+
+  const checkInputValidity = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+      showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+      hideInputError(formElement, inputElement);
+    }
+  };
+
+  const setEventListeners = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll(formSelectors.inputSelector));
+    const buttonElement = formElement.querySelector(formSelectors.submitButtonSelector);
+    toggleButtonState(inputList, buttonElement);
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', function () {
+        checkInputValidity(formElement, inputElement);
+        toggleButtonState(inputList, buttonElement);
+      });
+    });
+  };
+
+  const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    })
+  };
+
+  const toggleButtonState = (inputList, buttonElement) => {
+    if (hasInvalidInput(inputList)) {
+      buttonElement.classList.add(formSelectors.inactiveButtonClass);
+    } else {
+      buttonElement.classList.remove(formSelectors.inactiveButtonClass);
+    }
+  };
+
+  const enableValidation = () => {
+    const formList = Array.from(document.querySelectorAll('.form'));
+    formList.forEach((formElement) => {
+      formElement.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+      })
+      setEventListeners(formElement);
+    });
+  };
+
+  enableValidation(formSelectors);
+})();
