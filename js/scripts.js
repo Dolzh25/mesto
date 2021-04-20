f2ca19170e7762a0bf2e538fd8c5b624f4cbeef9
Dolzh25 <gidravlika87@yandex.ru>
2021-04-20 09:42:33 +0300

    Выносит данные о карточках в отдельный файл

--- js/scripts.js
+++ js/scripts.js
@@ -22,33 +22,6 @@
   const fullscreenImage = popupFullscreen.querySelector('.popup__image');
   const fullscreenCaption = popupFullscreen.querySelector('.popup__caption');
 
-  const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
-
   const getPopupInputValue = input => input.textContent;
 
   const fillProfileForm = () => {
